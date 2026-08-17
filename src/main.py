2121fe4d82b34b2267fa8fa0c679b737343fe5f5
Rapsssito <contact@rodrigomartin.dev>
2026-08-17 13:54:58 +0200

    feat: improve output completeness

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from collections.abc import Generator
 from dataclasses import dataclass
 
 import pandas as pd
+from compression import gzip
 
 from common import logger
 from common.models import ElectionType
@@ -32,7 +33,7 @@ def read_election_data(file_path: str) -> Generator[ElectionData]:
     df.sort_values(by=["year", "month"], inplace=True)
     for _, row in df.iterrows():
         if row["election_type"] == "europeas":
-            if (row["year"] == 1985 and row["month"] == 12):
+            if row["year"] == 1985 and row["month"] == 12:
                 # Missing data for European elections, skipping these entries
                 logger.warning(
                     f"Skipping {row['year']} European elections due to missing data in the source."
@@ -45,7 +46,9 @@ def read_election_data(file_path: str) -> Generator[ElectionData]:
                 int(row["month"]),
             )
         elif row["election_type"] == "municipales":
-            if (row["year"] == 1979 and row["month"] == 4) or (row["year"] == 1983 and row["month"] == 5):
+            if (row["year"] == 1979 and row["month"] == 4) or (
+                row["year"] == 1983 and row["month"] == 5
+            ):
                 # Missing data for municipal elections, skipping this entry
                 logger.warning(
                     f"Skipping {row['year']} municipal elections due to missing data in the source."
@@ -79,7 +82,15 @@ def main(args):
     else:
         logger.setLevel(logging.INFO)
 
-    with open(args.output_file, mode="w", newline="", encoding="utf-8") as f:
+    if args.output_file.endswith(".gz"):
+        logger.info(f"Output will be written to compressed file: {args.output_file}")
+    else:
+        logger.info(f"Output will be written to file: {args.output_file}")
+    with (
+        gzip.open(args.output_file, mode="wt", newline="", encoding="utf-8")
+        if args.output_file.endswith(".gz")
+        else open(args.output_file, mode="wt", newline="", encoding="utf-8") as f
+    ):
         writer = csv.writer(f)
         # Write header
         writer.writerow(
@@ -90,6 +101,8 @@ def main(args):
                 "month",
                 "acronym",
                 "name",
+                "municipality",
+                "province",
                 "order",
                 "substitute",
                 "elected",
@@ -107,17 +120,26 @@ def main(args):
                     election_data.election_type, election_data.year, election_data.month
                 )
                 for candidate in election.candidates:
+                    name = candidate.full_name if candidate.full_name else ""
+                    municipality = (
+                        candidate.municipality if candidate.municipality else ""
+                    )
+                    province = candidate.province if candidate.province else ""
+                    substitute = "1" if candidate.substitute else "0"
+                    elected = "1" if candidate.elected else "0"
                     writer.writerow(
                         [
-                            candidate.full_name,
+                            name,
                             election.type.value,
                             election.year,
                             election.month,
                             candidate.candidacy.acronym,
                             candidate.candidacy.name,
+                            municipality,
+                            province,
                             candidate.order,
-                            candidate.substitute,
-                            candidate.elected,
+                            substitute,
+                            elected,
                         ]
                     )
 
@@ -135,9 +157,9 @@ if __name__ == "__main__":
     arg_parser.add_argument(
         "--output_file",
         type=str,
-        help="Path to the output CSV file",
+        help="Path to the output CSV(.GZ) file",
         default=os.path.join(
-            os.path.dirname(__file__), "..", "data", "output_data.csv"
+            os.path.dirname(__file__), "..", "data", "output_data.csv.gz"
         ),
     )
     arg_parser.add_argument("--debug", action="store_true", help="Enable debug logging")
